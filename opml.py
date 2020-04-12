92ce69afb0c0ff8e9bac880eaee983752999b278
Rodrigo Amaral <rodrigoamaral@gmail.com>
2020-04-12 13:04:50 -0300

    Refactor OPML class

--- opml.py
+++ opml.py
@@ -2,42 +2,61 @@ import sys
 from lxml import etree
 
 
-def _is_valid_opml(root):
-    return len(root) > 1 and root[0].tag == "head" and root[1].tag == "body"
-
-
-def _is_leaf_node(e):
-    return len(e) == 0
-
-
 class InvalidOPMLException(Exception):
     pass
 
 
 class OPML:
+    """Represents a basic OPML structure for a list of feed links.
+    
+    """
     def __init__(self, filename):
-        self._tree = self._parse_opml(filename)
-        self._root = self._tree.getroot()
-
-        if not _is_valid_opml(self._root):
-            raise InvalidOPMLException(f"{filename} is an invalid OPML file!")
-
-        self.outlines = [e for e in self._root.iter("outline") if _is_leaf_node(e)]
+        self._sourcefile = filename
+        self._tree = self._parse_opml()
+        self._root = self._get_root()
+        self.outlines = [e for e in self._root.iter("outline") if self._is_leaf_node(e)]
 
 
-    def _parse_opml(self, filename):
-        with open(filename, "r") as f:
+    def __len__(self):
+        return len(self.outlines)
+
+
+    def __iter__(self):
+        return iter(self.outlines)
+
+
+    def _parse_opml(self):
+        with open(self._sourcefile, "r") as f:
             return etree.parse(f)
 
 
+    def _get_root(self):
+        root = self._tree.getroot()
+        
+        if not self._is_valid_opml(root):
+            raise InvalidOPMLException(f"{self._sourcefile} is an invalid OPML file!")
+
+        return root
+
+
+    def _is_valid_opml(self, root):
+        return len(root) > 1 and root[0].tag == "head" and root[1].tag == "body"
+
+
+    def _is_leaf_node(self, e):
+        return len(e) == 0
+
+
     def to_markdown(self):
+        """Retrieve links as a mardown list string."""
+
         return "\n".join(f"- [{item.get('text')}]({item.get('xmlUrl')})" for item in self.outlines)
 
 
     def to_html(self):
-        pass
+        """Retrieve links as a HTML list string."""
 
-
+        raise NotImplementedError("HTML support coming soon!")
 
 
 if __name__ == "__main__":
